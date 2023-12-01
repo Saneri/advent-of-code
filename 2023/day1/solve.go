d7d8bb15a87544b6817d16d0e80038beee938543
Santeri Suitiala <santeri.suitiala@almamedia.fi>
2023-12-01 18:35:40 +0200

    solve day1

---
+++ 2023/day1/solve.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func readInput() []string {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.Split(string(data), "\n")
+}
+
+func stringToNumber(str string) int {
+	wordToNumber := map[string]int{
+		"one": 1,
+		"two": 2,
+		"three": 3,
+		"four": 4,
+		"five": 5,
+		"six": 6,
+		"seven": 7,
+		"eight": 8,
+		"nine": 9,
+	}
+
+	if (len(str) > 1) {
+		number := wordToNumber[str]
+		if (number == 0){
+			log.Fatal("fatal: ", str)
+		}
+		return number
+	}
+	number, err := strconv.Atoi(str)
+	if (err != nil) {
+		log.Fatal(err)
+	}
+	return number
+}
+
+func main() {
+	data := readInput()
+	sum := 0
+	re := regexp.MustCompile("[1-9]|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)")
+	for _, line := range data {
+		// had to create this monster for loop because I found no ways of finding overlapping matches in go regex
+		digits := []string{}
+		for (len(line) > 0) {
+			str := re.FindString(line)
+			if (len(strings.TrimSpace(str)) > 0) {
+				digits = append(digits, str)
+			}
+			line = line[1:]
+		}
+		first := stringToNumber(digits[0])
+		second := stringToNumber(digits[len(digits)-1])
+		sum += first * 10 + second
+	}
+	fmt.Println(sum)
+}
