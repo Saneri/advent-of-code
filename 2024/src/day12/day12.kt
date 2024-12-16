0b7b51029b7764c974fd90db7f76af58b00c301c
Saneri <santeri.suitiala@gmail.com>
2024-12-16 20:24:55 +0200

    solve day12b

--- 2024/src/day12/day12.kt
+++ 2024/src/day12/day12.kt
@@ -3,13 +3,11 @@ package day12
 import readInput
 
 fun main() {
-    val map: Map<Pair<Int, Int>, Char> = readInput(12)
-        .flatMapIndexed { rowIndex, row ->
-            row.mapIndexed { colIndex, char ->
-                Pair(rowIndex, colIndex) to char
-            }
+    val map: Map<Pair<Int, Int>, Char> = readInput(12).flatMapIndexed { rowIndex, row ->
+        row.mapIndexed { colIndex, char ->
+            Pair(rowIndex, colIndex) to char
         }
-        .toMap()
+    }.toMap()
 
     var price = 0
     val visited = mutableSetOf<Pair<Int, Int>>()
@@ -18,7 +16,7 @@ fun main() {
         if (visited.contains(node)) return Pair(0, 0)
 
         visited.add(node)
-        var borders = 0
+        var corners = 0
         var area = 1
 
         val directions = listOf(
@@ -28,23 +26,40 @@ fun main() {
             Pair(node.first, node.second - 1),
         )
 
-        for (direction in directions) {
-            if (map[direction] == type) {
-                val (b, a) = iter(direction, type)
-                borders += b
-                area += a
-            } else {
-                borders++
+        val adjacentPairs = directions.indices.map { i ->
+            Pair(directions[i], directions[(i + 1) % directions.size])
+        }
+
+        for ((dir1, dir2) in adjacentPairs) {
+            // convex corners
+            if (map[dir1] != type && map[dir2] != type) {
+                corners++
+            }
+            // concave corners
+            val diagonal = Pair(
+                dir1.first + dir2.first - node.first,
+                dir1.second + dir2.second - node.second
+            )
+            if (map[dir1] == type && map[dir2] == type && map[diagonal] != type) {
+                corners++
             }
         }
 
-        return Pair(borders, area)
+        for (direction in directions) {
+            if (map[direction] == type) {
+                val (b, a) = iter(direction, type)
+                corners += b
+                area += a
+            }
+        }
+
+        return Pair(corners, area)
     }
 
     for (node in map.keys) {
         if (visited.contains(node)) continue
-        val (borders, area) = iter(node, map[node]!!)
-        price += borders * area
+        val (corners, area) = iter(node, map[node]!!)
+        price += corners * area
     }
     println(price)
 }
