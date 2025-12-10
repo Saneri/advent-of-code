5e32631d0ea3094839b30c4442008a911bcd8c04
Saneri <santeri.suitiala@gmail.com>
2025-12-10 19:00:03 +0200

    solve 5

---
+++ 2025/day5/solve.py
@@ -0,0 +1,28 @@
+with open('input.txt', 'r') as file:
+    lines = file.read().split('\n\n')
+
+ranges = [(int(x[0]), int(x[1])) for x in [x.split('-') for x in lines[0].split('\n')]]
+
+areas = [ranges[0]]
+for range in ranges[1:]:
+    overlapping = []
+    touched = False
+    for area in areas:
+        # range outside area
+        if range[1] < area[0] or range[0] > area[1]:
+            continue
+        overlapping.append(area)
+        
+    if overlapping:
+        start = min([area[0] for area in overlapping] + [range[0]])
+        end = max([area[1] for area in overlapping] + [range[1]])
+        for area in overlapping:
+            areas.remove(area)
+        areas.append((start, end))
+    if not overlapping and not touched:
+        areas.append(range)
+
+count = 0
+for area in areas:
+    count += area[1] - area[0] + 1
+print(count)
